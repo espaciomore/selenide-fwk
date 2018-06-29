a252b64c2bd191fda37a5677eda7e1c9599e95ad
Manuel A. Cerda <espaciomore@gmail.com>
2018-06-29 18:45:04 -0400

    Update AutoConfig.java

--- src/main/com/testproject/java/framework/config/AutoConfig.java
+++ src/main/com/testproject/java/framework/config/AutoConfig.java
@@ -3,33 +3,138 @@ package main.com.testproject.java.framework.config;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.google.gson.JsonElement;
-import com.google.gson.JsonIOException;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class AutoConfig.
+ */
 public final class AutoConfig
 {
-  public static JsonElement get(String path) throws JsonIOException, JsonSyntaxException, FileNotFoundException
-  {
-    final JsonParser parser = new JsonParser();
-    final JsonElement obj = parser.parse(new FileReader("local\\config\\config.properties"));
-    final JsonObject json = obj.getAsJsonObject();
-    
-    String[] arrayPath = path.split("\\.");
-    int len = arrayPath.length;
-    if(len > 1) {
-      JsonObject jo = json.get(arrayPath[0]).getAsJsonObject();
-      for(int i=1; i<len; i++) { 
-        if (i == len - 1) {
-          return jo.get(arrayPath[i]);
-        } else {
-          jo = jo.get(arrayPath[i]).getAsJsonObject();
-        }
-      }
-    }
-    
-    return json.get(path); 
-  }
+	
+	/** The Constant configBasePath. */
+	public static final String configBasePath = StringUtils.isEmpty(System.getProperty("configBasePath")) ? "" : System.getProperty("configBasePath");
+	
+	/** The Constant resourceBasePath. */
+	public static final String resourceBasePath = StringUtils.isEmpty(System.getProperty("resourceBasePath")) ? "" : System.getProperty("resourceBasePath");
+	
+	/** The Constant configFileUri. */
+	public static final String configFileUri = configBasePath + "config-properties.json";
+	
+	/** The data file uri. */
+	public static String dataFileUri = configBasePath + "test-data.json";
+	
+	/** The last file uri. */
+	private static String lastFileUri;
+	
+	/** The last json object. */
+	private static JsonObject lastJsonObject;
+	
+	/**
+	 * Gets the.
+	 *
+	 * @param p_fileUri the p file uri
+	 * @return the json object
+	 * @throws FileNotFoundException the file not found exception
+	 * @throws Exception the exception
+	 */
+	public static JsonObject get(String p_fileUri) throws FileNotFoundException, Exception
+	{
+		if (p_fileUri.equals(lastFileUri)) {
+			return lastJsonObject;
+		}
+		
+		final JsonParser v_parser = new JsonParser();
+		final JsonElement v_config = v_parser.parse(new FileReader(p_fileUri));
+		final JsonObject v_json = v_config.getAsJsonObject();
+		
+		lastFileUri = p_fileUri;
+		lastJsonObject = v_json;
+		
+		return v_json; 
+	}
+	
+	/**
+	 * Gets the.
+	 *
+	 * @param p_fileUri the p file uri
+	 * @param p_locator the p locator
+	 * @return the json element
+	 * @throws FileNotFoundException the file not found exception
+	 * @throws Exception the exception
+	 */
+	public static JsonElement get(String p_fileUri, String p_locator) throws FileNotFoundException, Exception
+	{
+		JsonObject v_json = get(p_fileUri);
+
+		String[] v_arrayPath = p_locator.split("\\.");
+		int v_len = v_arrayPath.length;
+		
+		JsonElement v_jsonElment = JsonNull.INSTANCE;
+		
+		for(int i=0; i<v_len; i++) { 
+			String key = v_arrayPath[i];
+			int index = -1;
+			// separate key from array index
+			if(key.endsWith("]")){
+				index = Integer.parseInt(key.replaceAll("^.*\\[(\\d+)\\]$", "$1"));
+				key = key.replaceAll("^(.*)\\[\\d+\\]$", "$1");
+			}
+			JsonElement this_je = v_json.get(key);	
+			// get json element by index for a json array
+			if(this_je.isJsonArray() && index >= 0){
+				v_jsonElment = this_je.getAsJsonArray().get(index);
+				this_je = v_jsonElment;
+			}
+			
+			if(this_je.isJsonArray()){
+				return this_je;
+			} else if(this_je.isJsonNull()){
+				return this_je;
+			} else if(this_je.isJsonObject()) {
+				v_json = this_je.getAsJsonObject();
+				continue;
+			} else if(this_je.isJsonPrimitive()){
+				return this_je;
+			} else {
+				System.out.println("Unkown JSON Identifier for '"+p_locator+"' from Json file ("+p_fileUri+")");
+				break;
+			}
+		}
+		
+		return v_jsonElment;
+	}
+	
+	/**
+	 * Read config.
+	 *
+	 * @param p_locator the p locator
+	 * @return the json element
+	 * @throws FileNotFoundException the file not found exception
+	 * @throws Exception the exception
+	 */
+	public static JsonElement readConfig(String p_locator) throws FileNotFoundException, Exception
+	{
+		return get(configFileUri, p_locator); 
+	}
+	
+	/**
+	 * Read data.
+	 *
+	 * @param p_locator the p locator
+	 * @return the json element
+	 * @throws FileNotFoundException the file not found exception
+	 * @throws Exception the exception
+	 */
+	public static JsonElement readData(String p_locator) throws FileNotFoundException, Exception
+	{
+		return get(dataFileUri, p_locator); 
+	}
 }
+
+
